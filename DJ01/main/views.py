7c18a89e6ce20811b43c2deb21723bfd9d9a6910
Chepurnov.S.S <CHEPURNOV.S.S@mail.ru>
2025-06-11 19:05:23 +0600

    Выполнен урок DJ01

---
+++ DJ01/main/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("<h1>Это мой первый проект на Django</h1>")
+
+def new(request):
+    return HttpResponse("<h1>Это вторая страница моего проекта на Django</h1>")
+
+def data(request):
+    return HttpResponse("<h1>Данные проекта</h1>"
+                        "<p>На этой странице вы можете ознакомиться с основными данными вашего проекта.</p>"
+                        "<p>Количество пользователей: 1234</p>"
+                        "<p>Активных сессий: 89</p>"
+                        "<p>Последнее обновление: 2025-04-05</p>")
+
+def test(request):
+    return HttpResponse("<h1>Тестовая страница</h1>"
+                        "<p>Эта страница используется для тестирования новых функций проекта.</p>"
+                        "<p>Текущий статус тестов: ✅ Все тесты пройдены</p>"
+                        "<p>Время последнего тестирования: 12:45</p>"
+                        "<p>Средняя скорость отклика: 230 мс</p>")
+# Create your views here.
